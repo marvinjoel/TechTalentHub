fbc555a877eb257fcfe97393d2e69cee80fcf746
MarvinAchas <marvinacha@ghlperu.com>
2025-01-04 23:14:27 -0500

    first proyect

---
+++ src/main/java/com/TechTalentHub/tech_talent_hub/TechTalentHubApplication.java
@@ -0,0 +1,13 @@
+package com.TechTalentHub.tech_talent_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TechTalentHub/tech_talent_hub/TechTalentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechTalentHub.tech_talent_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
