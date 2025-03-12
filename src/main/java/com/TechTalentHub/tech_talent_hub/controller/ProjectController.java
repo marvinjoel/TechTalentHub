263fd644bb33920431b8d1fc6bff249cb1fe3c57
MarvinAchas <marvinacha@ghlperu.com>
2025-03-12 17:06:20 -0500

    feat: adding profile crud

---
+++ src/main/java/com/TechTalentHub/tech_talent_hub/controller/ProjectController.java
@@ -0,0 +1,51 @@
+package com.TechTalentHub.tech_talent_hub.controller;
+
+import com.TechTalentHub.tech_talent_hub.model.Profiles;
+import com.TechTalentHub.tech_talent_hub.model.Project;
+import com.TechTalentHub.tech_talent_hub.service.ProfileService;
+import com.TechTalentHub.tech_talent_hub.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProfileService profileService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllProjects(){
+        return ResponseEntity.ok(projectService.findAllProject());
+    }
+
+    @PostMapping("/{profileId}")
+    public ResponseEntity<?> saveProjectForProfile(@PathVariable Long profileId, @RequestBody Project project){
+        Profiles profile = profileService.findByID(profileId);
+
+        project.setProfile(profile);
+        return new ResponseEntity<>(projectService.saveProject(project), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProjectById(@PathVariable Long id){
+        return ResponseEntity.ok(projectService.findByID(id));
+    }
+
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<?> getProjectsByProfileId(@PathVariable Long profileId){
+        List<Project> projects = projectService.findProjectsByProfileId(profileId);
+        if (projects.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No hay proyectos para este perfil.");
+        }
+
+        return ResponseEntity.ok(projects);
+    }
+}
